fc27e76b35e8b186e2b465d21cb8138e7876425e
Faisal <fhoda@users.noreply.github.com>
2023-04-11 13:54:15 -0500

    Update __init__.py

--- mlflow_provider/__init__.py
+++ mlflow_provider/__init__.py
@@ -1,6 +1,6 @@
 ## This is needed to allow Airflow to pick up specific metadata fields it needs for certain features. We recognize it's a bit unclean to define these in multiple places, but at this point it's the only workaround if you'd like your custom conn type to show up in the Airflow UI.
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 def get_provider_info():
     return {
