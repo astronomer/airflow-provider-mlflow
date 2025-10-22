a8c11ba3311f07f57c8d7e37251ea6f07e2f1ac6
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-10-22 16:50:53 +0100

    Attempt of new release of MLFlow provider

--- mlflow_provider/__init__.py
+++ mlflow_provider/__init__.py
@@ -1,9 +1,10 @@
 ## This is needed to allow Airflow to pick up specific metadata fields it needs for certain features.
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 def get_provider_info():
     return {
+        "classifier": "Development Status :: 7 - Inactive",
         "package-name": "airflow-provider-mlflow", # Required
         "name": "MLflow", # Required
         "description": "An MLflow Provider Package for Apache Airflow.", # Required
